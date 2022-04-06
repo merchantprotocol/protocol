cc908e26da682c5482c2c380813394da4e84f1d8
Jonathon Byrdziak <jonathonbyrd@gmail.com>
2022-04-06 14:18:44 -0700

    Quick config:save command

---
+++ src/Commands/ConfigSave.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * MIT License
+ * 
+ * Copyright (c) 2019 Merchant Protocol
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * 
+ * @category   merchantprotocol
+ * @package    merchantprotocol/protocol
+ * @copyright  Copyright (c) 2019 Merchant Protocol, LLC (https://merchantprotocol.com/)
+ * @license    MIT License
+ */
+namespace Gitcd\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Gitcd\Helpers\Shell;
+use Gitcd\Helpers\Config;
+use Gitcd\Helpers\Str;
+use Gitcd\Helpers\Dir;
+use Gitcd\Helpers\Git;
+use Gitcd\Utils\Json;
+use Gitcd\Utils\JsonLock;
+
+Class ConfigSave extends Command {
+
+    protected static $defaultName = 'config:save';
+    protected static $defaultDescription = 'Saves the current environment to the remote';
+
+    protected function configure(): void
+    {
+        // ...
+        $this
+            // the command help shown when running the command with the "--help" option
+            ->setHelp(<<<HELP
+            Saves the current environment to the remote repo.
+
+            HELP)
+        ;
+        $this
+            // configure an argument
+            // ...
+        ;
+    }
+
+    /**
+     * 
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return integer
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $helper = $this->getHelper('question');
+
+        // make sure we're in the application repo
+        $repo_dir = Git::getGitLocalFolder();
+        if (!$repo_dir) {
+            $output->writeln("<error>This command must be run in the application repo.</error>");
+            return Command::SUCCESS;
+        }
+
+        // make sure we have a config repo to start with
+        $configrepo = Json::read('configuration.local', false);
+        if (!$configrepo) {
+            $output->writeln("<error>Please run `protocol config:init` before using this command.</error>");
+            return Command::SUCCESS;
+        }
+
+        Git::commit( 'Saving untracked changes', $configrepo );
+        Git::push( $configrepo );
+        $output->writeln("<info>Your config repo was saved</info>");
+        return Command::SUCCESS;
+    }
+
+}
