6c04e60c57ccdeae25ac5768f0e5e79564442ed7
Jonathon Byrdziak <205828+jonathonbyrdziak@users.noreply.github.com>
2022-04-15 08:29:29 -0700

    Update global.php

--- config/global.php
+++ config/global.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'shell' => [
-        'outputfile' => '/var/log/protocol/background_process.log',
+        'outputfile' => '/var/log/protocol_background_process.log',
     ],
     'localdir' => '/opt/public_html',
     'banner_file' => 'templates/banner/motd.sh'
