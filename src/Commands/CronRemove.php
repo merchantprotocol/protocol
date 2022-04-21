91026c6b28d192b1203c3e93d386e6a9ac471475
Jonathon Byrdziak <jonathonbyrd@gmail.com>
2022-04-21 16:23:52 -0700

    Method to add the cron and remove the cron command

---
+++ src/Commands/CronRemove.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * MIT License
+ * 
+ * Copyright (c) 2019 Merchant Protocol
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * 
+ * @category   merchantprotocol
+ * @package    merchantprotocol/protocol
+ * @copyright  Copyright (c) 2019 Merchant Protocol, LLC (https://merchantprotocol.com/)
+ * @license    MIT License
+ */
+namespace Gitcd\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\LockableTrait;
+use Gitcd\Helpers\Shell;
+use Gitcd\Helpers\Dir;
+use Gitcd\Helpers\Git;
+use Gitcd\Helpers\Crontab;
+use Gitcd\Utils\Json;
+
+Class CronRemove extends Command {
+
+    use LockableTrait;
+
+    protected static $defaultName = 'cron:remove';
+    protected static $defaultDescription = 'Removes the cron command';
+
+    protected function configure(): void
+    {
+        // ...
+        $this
+            // the command help shown when running the command with the "--help" option
+            ->setHelp(<<<HELP
+            Removes the cronjob to restart protocol.
+    
+            ```
+            @reboot php protocol restart <repo>
+            ```
+
+            HELP)
+        ;
+        $this
+            // configure an argument
+            ->addArgument('local', InputArgument::OPTIONAL, 'The path to your local git repo')
+            // ...
+        ;
+    }
+
+    /**
+     * We're not looking to remove all changed and untracked files. We only want to overwrite local
+     * files that exist in the remote branch. Only the remotely tracked files will be overwritten, 
+     * and every local file that has been here was left untouched.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return integer
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        // command should only have one running instance
+        if (!$this->lock()) {
+            $output->writeln('The command is already running in another process.');
+
+            return Command::SUCCESS;
+        }
+
+        // the .git directory
+        $repo_dir   = $input->getArgument('local') ?: Git::getGitLocalFolder();
+
+        // add crontab restart command
+        $output->writeln('<info>Removing cron job</info>');
+        Crontab::removeCrontabRestart( $repo_dir );
+        $output->writeln('Checking if cronjob was removed: '. (!Crontab::hasCrontabRestart( $repo_dir ) ?"<info>YES</info>" :"<comment>NO</comment>"));
+
+        return Command::SUCCESS;
+    }
+
+}
